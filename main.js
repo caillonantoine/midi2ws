17869188e2caad1c8141fa5247d629d5848baa56
acaillon <acaillon@google.com>
2024-02-08 16:40:22 +0000

    Show midi msg without connection

--- main.js
+++ main.js
@@ -2,26 +2,37 @@ const midi = require("midi");
 const websockets = require("ws");
 const prompt = require("prompt-sync")({ sigint: true });
 
-async function main() {
-  const input = new midi.Input();
+let ccList = [];
 
+async function main() {
+  // select and open midi port
+  const input = new midi.Input();
   console.log("Available midi inputs:");
   for (let i = 0; i < input.getPortCount(); i++) {
     console.log(i, "--", input.getPortName(i));
   }
   let port = prompt("Select a MIDI input: ");
   port = Number(port);
-
   input.openPort(port);
+  input.on("message", (deltaTime, message) => {
+    let ccIndex = ccList.indexOf(message[1]);
+    if (ccIndex == -1) {
+      console.log("CC", message[1], "->", message[2]);
+      ccList.push(message[1]);
+      setTimeout(() => {
+        ccList.splice(ccList.indexOf(message[1]), 1);
+      }, 100);
+    }
+  });
   console.log("listening to port", input.getPortName(port));
 
+  // starting websocket server
   const wss = new websockets.WebSocketServer({ port: 8080 });
   console.log("waiting for connection (ws://localhost:8080)");
   wss.on("connection", (ws) => {
-    let ccList = [];
     console.log("connection established !");
     input.on("message", (deltaTime, message) => {
-      ccIndex = ccList.indexOf(message[1]);
+      let ccIndex = ccList.indexOf(message[1]);
       if (ccIndex == -1) {
         ws.send(JSON.stringify(message));
         console.log("CC", message[1], "->", message[2]);
