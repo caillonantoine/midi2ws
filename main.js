d2a00893db00705ecedcc93eec5a7a8da4cd8ffe
acaillon <acaillon@google.com>
2024-02-26 14:45:47 +0100

    Add ableton link support

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-const midi = require("midi");
-const websockets = require("ws");
-const prompt = require("prompt-sync")({ sigint: true });
+import midi from "midi";
+import { WebSocketServer } from "ws";
+import abletonlink from "abletonlink";
+import prompt from "prompt-sync";
+
+const link = new abletonlink();
+const wss = new WebSocketServer({ port: 8080 });
 
 let ccList = [];
 let rateLimiter = 40;
 
-async function main() {
-  // select and open midi port
+function getMidiInput() {
   const midiInput = new midi.Input();
   console.log("Available midi inputs:");
   for (let i = 0; i < midiInput.getPortCount(); i++) {
     console.log(i, "--", midiInput.getPortName(i));
   }
-  let port = prompt("Select a MIDI midiInput: ");
+  let port = prompt({ sigint: true })("Select a MIDI midiInput: ");
   port = Number(port);
   midiInput.openPort(port);
   midiInput.on("message", (deltaTime, message) => {
@@ -20,12 +23,32 @@ async function main() {
     console.log("CC", message[1], "->", message[2]);
   });
   console.log("listening to port", midiInput.getPortName(port));
+  return midiInput;
+}
+
+function sendLinkState(ws) {
+  link.update();
+  ws.send(
+    JSON.stringify({
+      type: "linkState",
+      beat: link.beat,
+      phase: link.phase,
+      bpm: link.bpm,
+      time: Date.now(),
+    })
+  );
+}
+
+async function main() {
+  // select and open midi port
+  let midiInput = getMidiInput();
 
   // starting websocket server
-  const wss = new websockets.WebSocketServer({ port: 8080 });
   console.log("waiting for connection (ws://localhost:8080)");
+
   wss.on("connection", (ws) => {
     console.log("connection established !");
+    // midi related handlers
     midiInput.removeAllListeners();
     midiInput.on("message", (deltaTime, message) => {
       if (message[0] !== 176) return;
@@ -37,6 +60,15 @@ async function main() {
         setTimeout(() => {
           ccList.splice(ccList.indexOf(message[1]), 1);
         }, rateLimiter);
+      }
+    });
+
+    // metadata related handlers
+    ws.on("message", (data) => {
+      data = JSON.parse(data.toString());
+      switch (data.type) {
+        case "linkState":
+          sendLinkState(ws);
       }
     });
   });
